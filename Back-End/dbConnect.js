d93b9f2e8967b8951d88647eb786c0ffba9d706a
AelinaXY <Lauren@2infinity.net>
2023-05-09 12:29:19 +0100

    Add Broken dbconnection

--- Back-End/dbConnect.js
+++ Back-End/dbConnect.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql');
-const details = require(/*dbconfig.js*/);
-const connection = mysql.createConnection({/*ENTER DETAILS*/})
+const dbconfig = require('./dbconfig.js');
+console.log(dbconfig);
+const connection = mysql.createConnection(dbconfig);
+connection.connect();
 
+connection.query('select * from cinema.films', (err, rows, fields) => {
+    if (err) throw err
+  
+    console.log(rows);
+  })
 
-module.exports(connection)
+module.exports = (connection)
